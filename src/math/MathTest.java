53bfe73891fbb07a201a78e4ced12160f5aace3c
Howard Fei <ffhhfyh@gmail.com>
2015-03-10 16:01:34 -0400

    New MathTest class

---
+++ src/math/MathTest.java
@@ -0,0 +1,17 @@
+package math;
+
+public class MathTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = (int)Math.round(42.2);
+		int y = Math.min(22,33);
+		int z = Math.abs(-11);
+		
+		System.out.println("" + x);
+		System.out.println("" + y);
+		System.out.println("" + z);
+
+	}
+
+}
