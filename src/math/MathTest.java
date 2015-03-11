eadd1928228a2ac2948b4640e9c51485a98c6f56
Howard Fei <ffhhfyh@gmail.com>
2015-03-11 15:14:31 -0400

    Added Java API.

--- src/math/MathTest.java
+++ src/math/MathTest.java
@@ -4,12 +4,14 @@ public class MathTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int x = (int)Math.round(42.2);
+		int x = Math.round(42.2f);
 		int y = Math.min(22,33);
+		int a = Math.max(22,33);
 		int z = Math.abs(-11);
 		
-		System.out.println("" + x);
+		System.out.println("" + x); 
 		System.out.println("" + y);
+		System.out.println("" + a);
 		System.out.println("" + z);
 	}
 
---
+++ src/primitiveWrapping/Wrapping.java
@@ -0,0 +1,11 @@
+package primitiveWrapping;
+import java.util.ArrayList;
+
+public class Wrapping {
+	public static void main(String[] args){
+		int x = 32;
+		ArrayList list = new ArrayList();
+		list.add(x);
+		System.out.println(list.get(0));
+	}
+}
