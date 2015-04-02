06e03f292bc4bb89bc9361e95262faaf65483b1d
Howard Fei <ffhhfyh@gmail.com>
2015-04-02 16:30:00 -0400

    new editting added

--- src/calendar/FullMoons.java
+++ src/calendar/FullMoons.java
@@ -1,7 +1,22 @@
 package calendar;
+import static java.lang.System.out;
+import java.util.Calendar;
+import java.lang.String;
 
 public class FullMoons {
+	static int DAY_IM = 1000*60*60*24;
 	public static void main(String[] args) {
-		
+		Calendar c = Calendar.getInstance();
+		c.set(2004,0,7,15,40);
+		out.println(c.getTime());
+		long day1 = c.getTimeInMillis();
+		out.println(day1);
+		for(int x = 0; x < 60; x++){
+			day1 += DAY_IM * 29.52;
+			c.setTimeInMillis(day1);
+//			String output = String.format("full moon on %ta %<tb %<td %<tT %<tZ %<tY", c.getTime());
+			String output = String.format("full moon on %tc", c);
+			out.println(output);
+		}
 	}
 }
--- src/test/Test.java
+++ src/test/Test.java
@@ -4,8 +4,17 @@ import java.util.Arrays;
 
 public class Test {
 	public static void main(String[] args) {
-		int[] a = {1,2,3,4,5,6};
-		int[] b = Arrays.copyOfRange(a,0,4);
-		System.out.println(Arrays.toString(b));
+//		int[] a = {1,2,3,4,5,6};
+//		int[] b = Arrays.copyOfRange(a,0,4);
+//		String c = "";
+//		for (int i = 0; i < a.length; i++) {
+//			c += a[i];
+//		}
+//		System.out.println(Arrays.toString(b));
+//		System.out.println(Arrays.toString(a));
+//		System.out.println(c);
+		String s = "abcdefg";
+		char[] chars = s.toCharArray();
+		System.out.println("" + chars[0]+ chars[1]);
 	}
 }
